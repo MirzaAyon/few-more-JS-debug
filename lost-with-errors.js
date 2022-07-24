b9bb5b7f4a332a564c69f7e557d233f15b821354
MirzaAyon <ayonmirza98@gmail.com>
2022-07-25 01:50:04 +0600

    lost errors added

---
+++ lost-with-errors.js
@@ -0,0 +1,40 @@
+//record count e click korle show kore 14 ta but ache 9 ta
+//chart e problem tai firwst e chart er btn e inspact
+//tmn kichu show hyna
+//tai coder modhe con+f diye chart dilam
+//32 ta item showng ja long project er jnno difficult
+//tai randomly ektae bp dibo
+//8 no line e bp dilam
+//control+shift+o diye chart name func gula ber korlam
+//get chart e giye
+//30 no e graph e ekta bp dilam
+//relod marlam ete ek ta na ekta place e theme jabei]
+//bp gulake minimize korlam
+//dane arekbar pause on bp te click korlam
+//drawchart name option ashbe
+//stack e drwa chart ke call kortese initialization chart
+//ui te koyta data ache eta age dekhbo ejnno ui er take continue kore dilam
+//ache 9 ta data show kore 14 ta abr record count e click korlam
+//8 no line e datar upr hover korlam
+//data gula object akare ache karon {}
+//data gula console e dekhbo data.length dile undefined ashe
+//Array.isArray(data)
+//ans ashbe false
+//tar mane eta array na
+//keys method apply korbo
+// Object.keys(data)
+//eta dibo
+//14 ta keys ache ta show korbe
+// Object.keys(data).length
+// 14 ta ache
+//ebar 4 no e bp dibo reload dibo
+//erpr dekhbo mouse hover kore je kothae kyta data ache
+//drachart e dhukar jnno f11 marbo
+//30 no line e niye jabe
+//abar 3/4 rakhbo sudhu
+//and 3 er datay hover kore dekhbo 14 ta data ache
+// console.table(data, ['name','salary'])
+//console e eta kore dekhbo je jhanker er onk gula data ache
+//3 theke f11 diye 2 bar nambo firse 8 no line e and pore 23 no line e jabe
+//26 e akta bp diye daan theke uncheck kore dile 26 e eshe thambe
+//erpr f10 dile shekhan theke return hoye 10 no e jabe
