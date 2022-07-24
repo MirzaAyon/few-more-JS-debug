866c968e29d8b938e63a8e022ec866a37ae70638
MirzaAyon <ayonmirza98@gmail.com>
2022-07-24 12:38:06 +0600

    first commit

---
+++ 1-intro.js
@@ -0,0 +1,27 @@
+//salary app repo download
+//pura salary app ta dekhbo code gula
+//elements e kono btn khujte hole shei btn er upr inspact korle btn er elements link chole ashbe
+//and picker on kore btn er upr click korleo btn dekhabe
+//echara html er shb code elements e ache
+//picker on kore move korleo shob html er jinish ke indicate korbe
+//and picker chara code e mouse ghurale website er jinish gula indicate hbe
+//daan pasher styleichcha moto change kora jabe
+//pore control+z dilei hbe
+//foont size barano komanor jnno upper arrow down arrow dilei hbe
+//shift chepe upper arrow down arrow dile 10 kore bara koma hbe
+//btn ke select korle uporer egula btn er jnno hbe
+//daner cls e click korle class ki ki ache ta dekhabe
+//ekta cls ke uncheck kore add new class e arekta cls likhe enter dile new cls add hye jabe
+//computed er vitore css box model ache shekhan theke shob kichu dekha jabe and edit kora jabe
+//jodi html e flex or grid thake tahole layout e debug korar moto onk kichu thake
+//code er kotahe event listener add hoise ta amra ber kore felte pari
+//code e click kore event listener tab e gelei hbe
+//kothao bp thakle sheta breakpoint tab e show hbe
+//code er modhe cn+f diye .diye #diye ba normally kono kichu search kore ber kore fela jae
+//daane accessibility tab ache
+//echara device tab ache okhane bivinno device choose kora jae
+//view dewa jae like 30% to 100%
+//chaile rotate kora jae
+//daner three btns theke show ruler use kora jae
+//bivinno device choose kore three btns theke capture ss mara jae
+//esc diye nicher console ana jae
