c6904ed20de9e1e1d6cebf6e1516f80c03506ea8
MirzaAyon <ayonmirza98@gmail.com>
2022-07-24 22:54:34 +0600

    !true !false added

---
+++ 3-salary-app-reload.js
@@ -0,0 +1,24 @@
+//name and salary add kora jae 
+//ayon and 90000 dilam , ayon added but 90000 add hyni
+//tai inspact - console - kichue error ache
+//d3.js name ekta error achr
+//eke click krbo, eta amdr likha code na - eke niche {} btn hote pretty print korlam
+//pray 10k line er code ache and 7k line e error khaise
+//7807 no line e error tai ekhane ekta bp dilam
+//ekhn sakib 10000 diye try krbo uporer line e break khabe
+//watch, bp apatoto drkr nai tai egula apatoto bondho - right side e scope,local o bondho
+//call stack er modhe shob error ache
+//t3.transformed ke right click diye ignore korlam
+//karon libary to amra fix korte parbo na
+//sakib er naam add hoche but graph add hoche na
+//and upore baame kichu ekta dekha jachee je eta NaN mouse nilei bujha jabe
+//reload diye abr komola khatun 90000 addkorlam erpr btn tae inspact korlam ekta elements er btn showung shekhane id ache but kno onclick ache kina dekhar jonno daner addeventlistener e click dilam
+//click ashlo shetake expand korle salary app js er 82 no line func ache shetake click korle js er 82 line e chole jabo
+//chaile 82 line e bp diye test kora jae but amra ektu ponditi korbo
+//tai console e debug(addRecordHandler) dibo
+//undefined showing
+//ekhn komla khatun 90000 e click korbo site theke
+//tkhn thik e source er bp te niye jabe 83 no line e jekhane kono bp nai
+//f10 mere mere dekhbo name and salary ke pache kina and mouse hover krbo name r salary te
+//source and console ek sathe dekhar jnno esc marbo
+//console e name !name  diye try korbo ekta true ekta false ashbe ekhane ! = bang sign 
---
+++ 4-logical-not-double-not.js
@@ -0,0 +1,73 @@
+//ager code tar bp theke shorae dibo
+//pause to debug theke continue kore dibo
+//erpr abr reload mere dibo, jam lege gele sheta clear hoye jabe
+//debug(addRecordHandler) dile komola khatun add dile 83 no line e break hbe
+//sources er modhe f10/f11 option ache
+//console e
+//const myName = '';
+// undefined
+// !myName
+//eta dbo but ager error er jnno error khabe
+// tai debug pause take continue kore dbo
+//ekhn
+// const myName = ''; //empty string ke boolean e convert kore dekhse eta false
+// undefined
+// !myName //false er biporid holo true
+// true
+// dile true ashbe
+
+
+// let ayon = 5; //0 falsy but 5 truthy
+// undefined
+// !ayon
+// false
+
+// let ayon = 'fjr3jk'; //empty string falsy but with string truthy
+// !ayon
+// flase
+
+
+// let plao = undefined; //false
+// !plao; //true
+
+//reload dibo
+//console e abar debug
+// debug(addRecordHandler)
+//dibo
+
+
+// const ayon = '5'
+// undefined
+// !ayon
+// false
+// !!ayon
+// true
+
+//maan na dile shetake !value diye check kora hoe
+
+
+//abr console diye debugger on
+//f10 diye diye add record er modhe gelam //erpr f11 marlam
+//step into dewar por
+// const newItem = getRecord(name, salary);
+//ei line show hbe and daane salary false show hbe scope er mdhe
+//f10 diye diye salary_datar modhe gelam
+//erpr abr f11 dilam 3 bar er moto diye 3 no line hote 8 no line e ashbe
+//vitore jawa mane f11
+//7 no line e datar upr mouse nile dekha jabe data ta object akare ache
+//erpr console.table diye data gulake dekhar try korbo
+//console.table(data) data gulake dekhbo inspact e
+//dekha jabe shob salary thik ache but komola khatun er false
+//erpr sudhu name and salary dekhar jnno
+//console.table(data, ['name','salary'])
+//eta dbo
+//debug theke ber hoye gelam
+//abr debug dibo
+//f10 diye diye 91 line asha porjnto 90000 ahsbe
+//91 no line e dekha jabe 90000 pathaisi but ota ! mane false kore
+//abr f11 e giye 94 line e jabo
+//dekha jachee salary false ashche
+//salary false er joono code e giye ! ta uthae dbo
+//salary add e giye con+f diye !salary search diye eke salary kore dibo
+//ekhn komola khatun shundor moto add hye jabe 
+
