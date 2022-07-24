e30d9e8e36c5095736dc37d77c068e09433b0900
MirzaAyon <ayonmirza98@gmail.com>
2022-07-24 13:33:42 +0600

    console-tab added

---
+++ 2-console-tab.js
@@ -0,0 +1,34 @@
+//devtool console api
+//google
+//google er ta dekhbo
+//assert ki janbo
+//assert,clear,count,count reset,debug,console error,table ta dekhbo
+//query selector = dollar, qs all = double dollar
+//geteventlisteners(object)
+//keys(object)
+//monitor(function) //func er naam ta str akare dewa jbe na
+//debuf(fucton) dekhbo
+//debug er pore func er naam dilei nije bp dibe na nije nije breakpoint hoye jabe
+
+
+
+//ebar sources dekhbo
+//souces e giye kono ekta file - right click - close others dile onno gula choel jabe - and close right dile daner gula chole jabe
+//shob close hole control + p diye open file hbe
+//same vabe vs eo control+p diye search bar e search diye jkono file khula jae
+//same vabe close o kora jabe right and all
+//control+shift+t jeta close hoye gese sheta abr fire ashbe vs e
+//chorme e koekta tab ek sathe bondo kora jnno
+//ekta tab e click
+//shift diye 2/3 ghor pashe arkta tab e click
+//erpr control w chaple shob off hye jabe
+//control shift t marle abr shob back hbe
+//hard relaod - inspact on thaka obosthae reload e chepe dhore hard reload or empty cache reload dite hbe
+//source theke con+p diye salaryapp file open - 26 no line  e bp diboo and right click kore conditional bp dewa jabe jemon disable bp or edit bp
+//ekhane js er expression thakbe
+//return er age pore bp dewa jabe
+//ekadhik bp dewa jabe
+//daan pashe shb bp er list thakbe
+//func ber korar jnno control + shift + o
+//daan pashe shob kichu ache like xhr and bp
+//ellemnt,console,sources,network,application egula as a developer amr lagbe debug korte
