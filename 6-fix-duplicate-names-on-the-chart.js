883664b12dbfc3969ac2bb96b2ed228f8518a6b4
MirzaAyon <ayonmirza98@gmail.com>
2022-07-26 21:02:21 +0600

    some debugging method added

---
+++ 6-fix-duplicate-names-on-the-chart.js
@@ -0,0 +1,53 @@
+//4 and 26 e bp dewa
+//diye reload dibo
+//f10 dile 10 no line e niye jabe 26 theke
+//8 er chatitems er upr mouse hover korle dekha jae ekhno 13 ta ashtese
+//10 no line e to achi e ekhn ekhan theke f11 chapbo
+//225 no line e niye jabe
+//225 e ekta obj ache
+//etar upore comment ta porbo //comment e onk idea dewa ache
+//224 e hover korle 14 ta array ache
+//228 e ekta bp dilam
+//diye daan side theke chalae dilam
+//jehetu bp ache tai break khabe
+//227 e item e hover korle object show hbe
+//watch e giye uniquenames likhe search dibo
+//obj ashbe
+//duplicate ke remove korar kaj ta 231 line e hoche
+//228 e achi f10 marle unique names e jabo
+//item.name e hover korle naam ta ashbe andy osmani
+//watch e item.name dilam
+//ebar ek ke kore daner continue te click korle ek ek kore update hbe names
+//evabe ek ek korte korte ek shomoy jhankar ashbe
+//data kom ache tai emne korsi , data beshi thakle
+//but eta onk tough tai bp er baame condition diye dibo right click kore
+//ei kaj korar jnno ager bp gula off korbo
+//con+shift+o kore uniqify func ke ber krbo
+
+//new kore abr inspact korbo
+//231 e bp dibo
+//item.name.toLowerCase().includes('jhankar Mahbub')
+//item.name.includes('jhankar Mahbub')
+//ei code diye direct name ber korbo
+//daane name gula show hbe
+//ek ek kore f10 ba daner continue dbo
+//f10/continue marle bujha jachee same naam ache tao keno bar bar niche same namer kache chole jache
+//shob gula naam unique name ke expand korlei bujha jache
+// uniqueNames[item.name]
+// eta dile dekha jache "" showing jeta false
+// tar mane uniquenames shorter modhe jachchei na
+//kono ekta obj er modhe kono ekta property na thakle undefined show hbe
+//uniqueNames['abc'] inspact e dile undefined ashbe
+// if (uniqueNames[item.name] !== undefined) {
+    //ekhane !=undefined dibo
+    //228 no line e 
+    //uniqueNames[item.name] dile inspact e "" show hbe
+    //item.name dile jhanker show hbe 
+    //thakle !== undefined
+    //na thakle == undefined
+    //note chara ei video ta abr dekha better
+
+
+
+//video ta abar dekhbo
+//dekhe kaj krbo
---
+++ 7.js
@@ -0,0 +1,3 @@
+//empty string and undefined same na 
+//6 no ta dekhe dekhe abr practice korbo tahole best hbe 
+//dorkar porle 7 no ta abr dekhbo debug e help hbe prochur
